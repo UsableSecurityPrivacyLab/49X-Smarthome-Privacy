90663b97099816a20e3d4ef460359d2471395f5b
rodgere <rodgere@wwu.edu>
2023-05-22 14:55:08 -0700

    Working on queries in appjs

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const { Client } = require('pg')
 
-var data = null
-
+var data
  
 const client = new Client({
     user: 'aretha',
@@ -13,36 +12,6 @@ const client = new Client({
     password: 'password',
     port: 5432,
   })
-
-  client.connect((err) => {
-    if (err) {
-      console.error('connection error', err.stack)
-    } else {
-      console.log('connected')
-      console.log("==================================")
-
-      // 
-      // SELECTING ALL FROM GEODATA TABLE
-      // CURRENTLY ONLY SENDING BACK FIRST IP FROM rows[0]
-      //  
-      client.query('SELECT * FROM geodata', (err, res) => {
-
-        data = res.rows[0].ip;
-
-
-        console.log("Geodata table: ")
-        console.log(data)
-
-
-        console.log("==================================")
-        console.log("Ending connection...")
-        client.end()
-      })
-      
-    }
-  })
-
-
 //   
 // 
 // NODE JS SERVER CODE STARTS HERE
@@ -57,14 +26,60 @@ const port = 3000;
 
 const server = http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
-    if(path=="/getstring"){
+    if(path=="/getQuery"){
         console.log("request recieved");
-        console.log("string '" + data + "' chosen");
+      // QUERY 
+      // 
+      client.connect((err) => {
+        if (err) {
+          console.error('connection error', err.stack)
+        } else {
+          console.log('Connection to db successful!')
+          console.log("==================================")
+        }
+      })
+
+      // This needs to be waited on
+      // 
+      client.query('SELECT * FROM geodata', (err, res) => {
+
+        data = res.rows[0].ip;
+
+        console.log("Geodata table: ")
+        console.log(data)
+      })
+
+
         response.setHeader("Access-Control-Allow-Origin", 'http://127.0.0.1:5500');
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.end(data);
         console.log("string sent");
-    }else{
+
+
+        client.end();
+      console.log("==================================")
+      console.log("Ending db connection...")
+    }
+    else if(path =="/closeDB"){
+      // 
+      // CLOSE THE DB CONNECTION
+      client.end();
+      console.log("==================================")
+      console.log("Ending db connection...")
+    }
+    else if(path == "/openDB"){
+
+      client.connect((err) => {
+        if (err) {
+          console.error('connection error', err.stack)
+        } else {
+          console.log('Connection to db successful!')
+          console.log("==================================")
+        }
+      })
+
+    }
+    else{
         fs.readFile('./index.html', function(err, file) {  
             if(err) {  
                 // write an error response or nothing here  
