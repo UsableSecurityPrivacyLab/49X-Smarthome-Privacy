f939d1f7929c99abba52f09fcbe89e525949fb82
rodgere <rodgere@wwu.edu>
2023-05-23 16:12:10 -0700

    Queries are now waited on asynchronously.

--- app.js
+++ app.js
@@ -1,95 +1,73 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const { Client } = require('pg')
+const { Client } = require('pg');
 
-var data
- 
-const client = new Client({
-    user: 'aretha',
-    host: 'localhost',
-    database: 'test',
-    password: 'password',
-    port: 5432,
-  })
-//   
-// 
-// NODE JS SERVER CODE STARTS HERE
-// 
-// THIS NEEDS TO BE RUNNING IN ORDER TO FETCH THE IPS FROM THE DASHBOARD.
-// RUN WITH "node app.js" IN TERMINAL
-// 
+
+
+let connected = false;
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
-const server = http.createServer(function(request, response){
-    var path = url.parse(request.url).pathname;
-    if(path=="/getQuery"){
-        console.log("request recieved");
-      // QUERY 
-      // 
-      client.connect((err) => {
-        if (err) {
-          console.error('connection error', err.stack)
-        } else {
-          console.log('Connection to db successful!')
-          console.log("==================================")
-        }
-      })
-
-      // This needs to be waited on
-      // 
-      client.query('SELECT * FROM geodata', (err, res) => {
-
-        data = res.rows[0].ip;
-
-        console.log("Geodata table: ")
-        console.log(data)
-      })
+async function handleRequest(request, response) {
+  const path = url.parse(request.url).pathname;
+  if (path === '/getQuery') {
 
 
-        response.setHeader("Access-Control-Allow-Origin", 'http://127.0.0.1:5500');
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.end(data);
-        console.log("string sent");
+    const client = new Client({
+      user: 'aretha',
+      host: 'localhost',
+      database: 'test',
+      password: 'password',
+      port: 5432,
+    });
 
 
-        client.end();
-      console.log("==================================")
-      console.log("Ending db connection...")
+    console.log('request received');
+    try {
+      if (!connected) {
+        await client.connect();
+        connected = true;
+        console.log('====== Connection to db successful! ======');
+      }
+      const res = await client.query('SELECT * FROM geodata');
+      try {await client.end()
+        connected = false;
+        console.log("====== client disconnected ======" + "\n connected:" + connected)
+      } catch {
+        console.log("failed to disconnect client")
+        connected = true;
+      }
+  
+      const data = res.rows[0].ip;
+      console.log('Geodata table:');
+      console.log(data);
+
+      response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end(data);
+      console.log('string sent');
+      
+    } catch (err) {
+      console.error('Query error:', err);
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.end('Internal Server Error');
     }
-    else if(path =="/closeDB"){
-      // 
-      // CLOSE THE DB CONNECTION
-      client.end();
-      console.log("==================================")
-      console.log("Ending db connection...")
-    }
-    else if(path == "/openDB"){
+  } else {
+    fs.readFile('./index.html', function (err, file) {
+      if (err) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end('Internal Server Error');
+        return;
+      }
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(file, 'utf-8');
+    });
+  }
+}
 
-      client.connect((err) => {
-        if (err) {
-          console.error('connection error', err.stack)
-        } else {
-          console.log('Connection to db successful!')
-          console.log("==================================")
-        }
-      })
-
-    }
-    else{
-        fs.readFile('./index.html', function(err, file) {  
-            if(err) {  
-                // write an error response or nothing here  
-                return;  
-            }  
-            response.writeHead(200, { 'Content-Type': 'text/html' });  
-            response.end(file, "utf-8");  
-        });
-    }
-})
+const server = http.createServer(handleRequest);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
