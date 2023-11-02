9c52ff05cb9090ec9d4a83507e982fb21eec0ff6
Neal Schobbe <schobbn@wwu.edu>
2023-11-02 13:51:02 -0700

    updated deps to incl sqlalchemy, more work on insert-test-data.py

--- scripts/insert-test-data.py
+++ scripts/insert-test-data.py
@@ -1,12 +1,18 @@
 #! /usr/bin/env python3
 
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "db"))
-import databaseBursts
-FILE_PATH = os.path.dirname(os.path.abspath(__file__))
-DB_MANAGER = databaseBursts.dbManager()
+from sqlalchemy import create_engine
+import pandas as pd
 
-# Either need to create new table in schema matching brennan's test data, or
-# insert null values into the attributes in the nearest matching table -
-# (not sure if there's one that supersets brennan's data exactly, could be two inserts)
+# cook csv into pandas dataframe
+# massage dataframe into the right shape to match the pgsql table
+# use dataframe.to_sql() 
+
+#create an engine for sqlalchemy to use to connect to the database
+engine = create_engine("postgresql+psycopg2://david:491-Home%21privacy@localhost:5433/homeoverwatch")
+
+#read csv file into a pandas dataframe
+df = pd.read_csv('testdata.csv')
+
+
+
+
