93076f342983b8f2e184da065d930e32555a1ac0
evanistyping <evansrodgers@gmail.com>
2023-11-07 11:52:45 -0800

    Working on device list

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ async function handleRequest(request, response) {
       // The data we return will need to change based on the query...
       // This currently only works if we query row(s) from geodata table.
       var data = "";
-      console.log('Geodata table:');
 
       for (let i = 0; i < res.rows.length; i++) {
         data += res.rows[i].ip + "__";
@@ -78,7 +77,69 @@ async function handleRequest(request, response) {
       response.writeHead(500, { 'Content-Type': 'text/plain' });
       response.end('Internal Server Error');
     }
-  } else {
+  } else if (path === '/loadDevices'){
+
+    // Parse our query from request.url
+    //  
+    let detach = request.url.split('?'); // This separates the url into 2 parts: [/getQuery] and [SELECT_*_FROM_geodata]
+    let query1 = detach[1].split('_').join(' '); // This replaces '_' with ' '
+    query1 = query1.split('%27').join('\'');
+
+    console.log(query1) // Final result: 'SELECT * FROM geodata'
+
+
+    // Create a new Client for connection
+    const client = new Client({
+      user: 'david',
+      host: 'localhost',
+      database: 'homewatch',
+      password: '491-Home!privacy',
+      port: 5433,
+    });
+
+
+    console.log('request received');
+    try {
+      if (!connected) { // Connect to db if not already connected
+        await client.connect();
+        connected = true;
+        console.log('====== Connection to db successful! ======');
+      }
+      const res = await client.query(query1); // Call actual query
+      try {await client.end() // Disconnect from db
+        connected = false;
+        console.log("====== client disconnected ======")
+      } catch {
+        console.log("failed to disconnect client")
+        connected = true;
+      }
+  
+      // ================================================================
+      // The data we return will need to change based on the query...
+      // This currently only works if we query row(s) from geodata table.
+      var data = "";
+
+      for (let i = 0; i < res.rows.length; i++) {
+        data += res.rows[i].mac + "__";
+        data += res.rows[i].name + "~~";
+      }
+
+      console.log(data);
+      console.log('Loading Devices')
+
+      response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+      response.writeHead(200, { 'Content-Type': 'text/plain' });
+      response.end(data); // Return value
+      console.log('string sent');
+      
+    } catch (err) {
+      console.error('Query error:', err);
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.end('Internal Server Error');
+    }
+
+  }
+  else{
     fs.readFile('./index.html', function (err, file) {
       if (err) {
         response.writeHead(500, { 'Content-Type': 'text/plain' });
--- scripts/editTitle.js
+++ scripts/editTitle.js
@@ -43,3 +43,47 @@ btn2.addEventListener("click", function() {
     }
     
 });
+
+
+// Query devices table mac addresses & names
+function loadDevices() {
+
+    var query = "SELECT_mac,_name_FROM_devices;"
+
+    // Check for undefined or null
+    if (query === undefined) {
+        const err = "***** Empty Query.";
+        console.error(err);
+        return err;
+    }
+
+    // Check for proper format. No ' '.
+    if (query.includes(' ')) {
+        const err = "***** Query improper format. Replace all ' ' with '_' ";
+        console.error(err);
+        return err;
+    }
+
+
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "http://localhost:3000/loadDevices?" + query, true);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+            // Result stored in string
+            string = xmlhttp.responseText;
+
+            // console.log("FETCHED FROM Devices: " + string);
+
+            var rows = parseString(string);
+            console.log(rows);
+
+
+        }
+    }
+    xmlhttp.send();
+}
+
+// Call this whenever page is loaded
+// 
+// loadDevices();
